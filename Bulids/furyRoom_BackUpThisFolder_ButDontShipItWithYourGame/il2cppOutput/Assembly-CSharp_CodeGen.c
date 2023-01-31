b4907af9a13668e0f38fe578d7e4c02111def35b
FloThomas80 <70484942+FloThomas80@users.noreply.github.com>
2023-01-31 14:05:24 +0100

    added fracture package
    added fracture package and exemples,
    added one home made mesh "alienbox"

---
+++ Bulids/furyRoom_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,105 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void GameMenuController::Start()
+extern void GameMenuController_Start_m94D2472791B13DA992AC42BC0EE644BEC8BDCE85 (void);
+// 0x00000002 System.Void GameMenuController::PauseButtonPressed(UnityEngine.InputSystem.InputAction/CallbackContext)
+extern void GameMenuController_PauseButtonPressed_m66C91504E52C5F01948056485733EEEA1F4F7FAB (void);
+// 0x00000003 System.Void GameMenuController::ShowUiWristMenu()
+extern void GameMenuController_ShowUiWristMenu_m7AEE41FBA0B005ED69C24C8D1E4D1A4B21CF3BBC (void);
+// 0x00000004 System.Void GameMenuController::RestartGame()
+extern void GameMenuController_RestartGame_mED538581943EDADDD98E1D49A12B93A0B295E53E (void);
+// 0x00000005 System.Void GameMenuController::ExitGame()
+extern void GameMenuController_ExitGame_m117883ABB26245280DB37D8FE11B0F5092DA97EB (void);
+// 0x00000006 System.Void GameMenuController::.ctor()
+extern void GameMenuController__ctor_mDCC0AA00F4572D25A462C5A1C1CEF1C9DABE3117 (void);
+// 0x00000007 System.Void IDamageable::damage(System.Int32)
+// 0x00000008 System.Void IMovement::Move()
+// 0x00000009 System.Single IMovement::get_SpeedMovement()
+// 0x0000000A System.Void IMovement::set_SpeedMovement(System.Single)
+// 0x0000000B System.Single PlayerController::get_SpeedMovement()
+extern void PlayerController_get_SpeedMovement_m59B5BF927024BDBC745BBD301BE4E334806C64AB (void);
+// 0x0000000C System.Void PlayerController::set_SpeedMovement(System.Single)
+extern void PlayerController_set_SpeedMovement_mD1639469DBE69AC81CF4A8DEC8B931699A56318B (void);
+// 0x0000000D System.Void PlayerController::damage(System.Int32)
+extern void PlayerController_damage_m494C9826D29522CC9785496A5AD1B714145B012A (void);
+// 0x0000000E System.Void PlayerController::Move()
+extern void PlayerController_Move_m68BFC8B882D42955A212B23E7D1DDD56CC87DB09 (void);
+// 0x0000000F System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33 (void);
+// 0x00000010 System.Void Unity.Template.VR.XRPlatformControllerSetup::Start()
+extern void XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF (void);
+// 0x00000011 System.Void Unity.Template.VR.XRPlatformControllerSetup::.ctor()
+extern void XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E (void);
+static Il2CppMethodPointer s_methodPointers[17] = 
+{
+	GameMenuController_Start_m94D2472791B13DA992AC42BC0EE644BEC8BDCE85,
+	GameMenuController_PauseButtonPressed_m66C91504E52C5F01948056485733EEEA1F4F7FAB,
+	GameMenuController_ShowUiWristMenu_m7AEE41FBA0B005ED69C24C8D1E4D1A4B21CF3BBC,
+	GameMenuController_RestartGame_mED538581943EDADDD98E1D49A12B93A0B295E53E,
+	GameMenuController_ExitGame_m117883ABB26245280DB37D8FE11B0F5092DA97EB,
+	GameMenuController__ctor_mDCC0AA00F4572D25A462C5A1C1CEF1C9DABE3117,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	PlayerController_get_SpeedMovement_m59B5BF927024BDBC745BBD301BE4E334806C64AB,
+	PlayerController_set_SpeedMovement_mD1639469DBE69AC81CF4A8DEC8B931699A56318B,
+	PlayerController_damage_m494C9826D29522CC9785496A5AD1B714145B012A,
+	PlayerController_Move_m68BFC8B882D42955A212B23E7D1DDD56CC87DB09,
+	PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33,
+	XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF,
+	XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E,
+};
+static const int32_t s_InvokerIndices[17] = 
+{
+	4798,
+	3907,
+	4798,
+	4798,
+	4798,
+	4798,
+	0,
+	0,
+	0,
+	0,
+	4735,
+	3829,
+	3770,
+	4798,
+	4798,
+	4798,
+	4798,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	17,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
