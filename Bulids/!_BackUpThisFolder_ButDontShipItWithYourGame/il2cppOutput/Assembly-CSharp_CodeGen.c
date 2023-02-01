43b7a737cc59760086b8e0c1f8b3e15ac38c4788
FloThomas80 <70484942+FloThomas80@users.noreply.github.com>
2023-02-01 15:14:51 +0100

    Added eggs in cylinder (pretexte pour tester le stached)
    (try to stashed)

--- Bulids/!_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
+++ Bulids/!_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -12,26 +12,26 @@
 
 
 
-// 0x00000001 System.Void NotKineAnymore::OnCollisionEnter(UnityEngine.Collision)
-extern void NotKineAnymore_OnCollisionEnter_m8075264B55A35B202B4DC2DF4A2FE61FF824B48B (void);
-// 0x00000002 System.Void NotKineAnymore::.ctor()
-extern void NotKineAnymore__ctor_mFDD1D59508B497A1A97E8BE4454780EB32A805D2 (void);
-// 0x00000003 System.Void GameMenuController::Start()
+// 0x00000001 System.Void GameMenuController::Start()
 extern void GameMenuController_Start_m94D2472791B13DA992AC42BC0EE644BEC8BDCE85 (void);
-// 0x00000004 System.Void GameMenuController::PauseButtonPressed(UnityEngine.InputSystem.InputAction/CallbackContext)
+// 0x00000002 System.Void GameMenuController::PauseButtonPressed(UnityEngine.InputSystem.InputAction/CallbackContext)
 extern void GameMenuController_PauseButtonPressed_m66C91504E52C5F01948056485733EEEA1F4F7FAB (void);
-// 0x00000005 System.Void GameMenuController::ShowUiWristMenu()
+// 0x00000003 System.Void GameMenuController::ShowUiWristMenu()
 extern void GameMenuController_ShowUiWristMenu_m7AEE41FBA0B005ED69C24C8D1E4D1A4B21CF3BBC (void);
-// 0x00000006 System.Void GameMenuController::RestartGame()
+// 0x00000004 System.Void GameMenuController::RestartGame()
 extern void GameMenuController_RestartGame_mED538581943EDADDD98E1D49A12B93A0B295E53E (void);
-// 0x00000007 System.Void GameMenuController::ExitGame()
+// 0x00000005 System.Void GameMenuController::ExitGame()
 extern void GameMenuController_ExitGame_m117883ABB26245280DB37D8FE11B0F5092DA97EB (void);
-// 0x00000008 System.Void GameMenuController::.ctor()
+// 0x00000006 System.Void GameMenuController::.ctor()
 extern void GameMenuController__ctor_mDCC0AA00F4572D25A462C5A1C1CEF1C9DABE3117 (void);
-// 0x00000009 System.Void IDamageable::damage(System.Int32)
-// 0x0000000A System.Void IMovement::Move()
-// 0x0000000B System.Single IMovement::get_SpeedMovement()
-// 0x0000000C System.Void IMovement::set_SpeedMovement(System.Single)
+// 0x00000007 System.Void IDamageable::damage(System.Int32)
+// 0x00000008 System.Void IMovement::Move()
+// 0x00000009 System.Single IMovement::get_SpeedMovement()
+// 0x0000000A System.Void IMovement::set_SpeedMovement(System.Single)
+// 0x0000000B System.Void NotKineAnymore::OnCollisionEnter(UnityEngine.Collision)
+extern void NotKineAnymore_OnCollisionEnter_m8075264B55A35B202B4DC2DF4A2FE61FF824B48B (void);
+// 0x0000000C System.Void NotKineAnymore::.ctor()
+extern void NotKineAnymore__ctor_mFDD1D59508B497A1A97E8BE4454780EB32A805D2 (void);
 // 0x0000000D System.Single PlayerController::get_SpeedMovement()
 extern void PlayerController_get_SpeedMovement_m59B5BF927024BDBC745BBD301BE4E334806C64AB (void);
 // 0x0000000E System.Void PlayerController::set_SpeedMovement(System.Single)
@@ -48,8 +48,6 @@ extern void XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52
 extern void XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E (void);
 static Il2CppMethodPointer s_methodPointers[19] = 
 {
-	NotKineAnymore_OnCollisionEnter_m8075264B55A35B202B4DC2DF4A2FE61FF824B48B,
-	NotKineAnymore__ctor_mFDD1D59508B497A1A97E8BE4454780EB32A805D2,
 	GameMenuController_Start_m94D2472791B13DA992AC42BC0EE644BEC8BDCE85,
 	GameMenuController_PauseButtonPressed_m66C91504E52C5F01948056485733EEEA1F4F7FAB,
 	GameMenuController_ShowUiWristMenu_m7AEE41FBA0B005ED69C24C8D1E4D1A4B21CF3BBC,
@@ -60,6 +58,8 @@ static Il2CppMethodPointer s_methodPointers[19] =
 	NULL,
 	NULL,
 	NULL,
+	NotKineAnymore_OnCollisionEnter_m8075264B55A35B202B4DC2DF4A2FE61FF824B48B,
+	NotKineAnymore__ctor_mFDD1D59508B497A1A97E8BE4454780EB32A805D2,
 	PlayerController_get_SpeedMovement_m59B5BF927024BDBC745BBD301BE4E334806C64AB,
 	PlayerController_set_SpeedMovement_mD1639469DBE69AC81CF4A8DEC8B931699A56318B,
 	PlayerController_damage_m494C9826D29522CC9785496A5AD1B714145B012A,
@@ -70,8 +70,6 @@ static Il2CppMethodPointer s_methodPointers[19] =
 };
 static const int32_t s_InvokerIndices[19] = 
 {
-	3795,
-	4798,
 	4798,
 	3907,
 	4798,
@@ -82,6 +80,8 @@ static const int32_t s_InvokerIndices[19] =
 	0,
 	0,
 	0,
+	3795,
+	4798,
 	4735,
 	3829,
 	3770,
